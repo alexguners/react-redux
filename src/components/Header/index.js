9790e3d0103b911154c6f780e1d81e36ea6f7352
User <user@MacBook-Pro-de-User.local>
2020-02-07 23:48:59 -0300

    Layout cart and home page

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <img alt="RocketShoes" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
